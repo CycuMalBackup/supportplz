dd75cbfa10205222703597d0fa4391f3c3ce59b8
= <malas456@gmail.com>
2017-09-02 20:26:29 +0200

    working table !!!!!

--- scripts/getresponse.js
+++ scripts/getresponse.js
@@ -1,81 +1,121 @@
 window.onload = function () {
     const maxWeek = 12,
-        disabledClass = 'disabled';
+        disabledClass = "disabled",
+        guiltFree = "GUILT FREE";
     var currentWeek = 5,
         previousWeekButton = document.getElementsByClassName('js-week-previous')[0],
         nextWeekButton = document.getElementsByClassName('js-week-next')[0];
 
     var buildTable = function () {
         var tableHtml = "<table>";
-        tableHtml += getMealsTimeHtml();
-        tableHtml += getDailyMealPlan();
-
+        tableHtml += getTableHeader(1);
+        tableHtml += getMeals();
+        tableHtml += getDietType();
+        tableHtml += getWorkoutData();
         tableHtml += "</table>";
-return tableHtml;
+        return tableHtml;
     };
-var displayTable = function () {
-  var tableDisplayed = buildTable();
-  var tablePlaceHolder = document.getElementsByClassName("tablePlaceHolder")[0];
-  tablePlaceHolder.innerHTML = tableDisplayed;
-};
 
-    var getDailyMealPlan = function () {
-        var dailyMealPlanHtml = "";
-        for (var i = 0; i < mealPlan.length; i ++) {
-            var dailyPlan = mealPlan[i];
-            var dayNumber = parseInt(mealPlan.day) + (currentWeek * 7);
-            if (dailyPlan.dietType === "LOW CARB") {
-                dailyMealPlanHtml += getLowCarbDayTemplateHtml(dayNumber);
-            } else if (dailyPlan.dietType === "HIGH CARB") {
-                dailyMealPlanHtml += getHighCarbDayTemplateHtml(dayNumber);
-            } else {
-                dailyMealPlanHtml += getGuiltFreeDayTemplateHtml(dayNumber)
+    var displayTable = function () {
+        var tableDisplayed = buildTable();
+        var tablePlaceHolder = document.getElementsByClassName("tablePlaceHolder")[0];
+        tablePlaceHolder.innerHTML = tableDisplayed;
+    };
+
+    var getTableHeader = function (dayNumber) {
+        var tableHeader = "<tr>";
+        tableHeader += "<th></th>";
+        tableHeader += "<th>DAY <span class=\"week-day-number\">" + dayNumber++ + "</span></th>";
+        tableHeader += "<th>DAY <span class=\"week-day-number\">" + dayNumber++ + "</span></th>";
+        tableHeader += "<th>DAY <span class=\"week-day-number\">" + dayNumber++ + "</span></th>";
+        tableHeader += "<th>DAY <span class=\"week-day-number\">" + dayNumber++ + "</span></th>";
+        tableHeader += "<th>DAY <span class=\"week-day-number\">" + dayNumber++ + "</span></th>";
+        tableHeader += "<th>DAY <span class=\"week-day-number\">" + dayNumber++ + "</span></th>";
+        tableHeader += "<th>DAY <span class=\"week-day-number\">" + dayNumber++ + "</span></th>";
+        tableHeader += "</tr>";
+        return tableHeader;
+    };
+
+    var getMeals = function () {
+        var meals = "";
+        for (var i = 0; i < mealHours.length; i++) {
+            meals += "<tr>";
+            meals += getMealsByHour(mealHours[i], i);
+            meals += "</tr>"
+        }
+        return meals;
+    };
+
+    var getMealsByHour = function (hour, hourId) {
+        var mealsByHours = "<td>" + hour + "</td>";
+        for (var index = 0; index < mealPlan.length; index++) {
+            var dayPlan = mealPlan[index],
+                hourSpecificMeal = dayPlan.meals[hourId];
+            if (dayPlan.dietType !== guiltFree) {
+                mealsByHours += getMealHtml(hourSpecificMeal, dayPlan.day === "2");
             }
-        } return dailyMealPlanHtml;
+            else if (hour === mealHours[0]) {
+                mealsByHours += getGuiltFreeDayTemplateHtml();
+            }
+        }
+        return mealsByHours;
     };
 
-    var getMealsTimeHtml = function () {
-        var tableHours =
-            "<tr><th></th></tr>" +
-            "<tr><td>6:00 AM</td></tr>" +
-            "<tr><td>9:00 AM</td></tr>" +
-            "<tr><td>12:00 PM</td></tr>" +
-            "<tr><td>3:00 PM</td></tr>" +
-            "<tr><td>6:00 PM</td></tr>" +
-            "<tr><td></td></tr>" +
-            "<tr><td><span>Workout</span><img src='../images/workoutArrow.png' alt='arrow'></td></tr>";
-        return tableHours;
+    var getMealHtml = function (hourSpecificMeal, currentDay) {
+        var mealId = hourSpecificMeal.mealId,
+            wasEaten = hourSpecificMeal.eaten,
+            mealName = hourSpecificMeal.meal;
+
+        var mealHtml = "<td>";
+        mealHtml += mealName;
+        if (wasEaten === "true") {
+            mealHtml += "<img class='was-eaten' src='./images/mealEaten.png' alt='tick'>";
+        }
+
+        if (mealId === 1) {
+            if (currentDay === true) {
+                mealHtml += "<img src=\"./images/todaysShake.png\" alt=\"shake\">";
+            } else {
+                mealHtml += "<img src=\"./images/shake.png\" alt=\"shake\">";
+            }
+        }
+        mealHtml += "</td>";
+        return mealHtml;
     };
 
-    var getLowCarbDayTemplateHtml = function(dayNumber) {
-        var lowCarbDay =
-            "<tr><th>DAY <span class=\"week-day-number\">" + dayNumber + "</span></th></tr>" +
-            "<tr><td>Bod•ē Shake <img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'><span class='shake-img'></span></td></tr>" +
-            "<tr><td>Ham and Swiss Roll Ups<img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'></td></tr>" +
-            "<tr><td>Turkey Melt<img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'></td></tr>" +
-            "<tr><td>Bod•ē Burn with Mozzarella and Tomato Slices<img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'></td></tr>" +
-            "<tr><td>Turkey Melt<img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'></td></tr>" +
-            "<tr><td>LOW-CARB</td></tr>" +
-            "<tr><td><span class='workout-done'></span></td></tr>";
-        return lowCarbDay;
+    var getDietType = function () {
+        var dietType = "<tr>";
+        dietType = "<td></td>";
+        for (var i = 0; i < mealPlan.length; i++) {
+            var dailyMealPlan = mealPlan[i];
+            if (dailyMealPlan.dietType === guiltFree) {
+                dietType += "<td rowspan=\"2\"><img src=\"./images/print.png\" alt=\"print\"/><span>Print</span></td>";
+            } else {
+                dietType += "<td>" + dailyMealPlan.dietType + "</td>";
+            }
+        }
+        dietType += "</tr>"
+        return dietType;
     };
-    var getHighCarbDayTemplateHtml = function(dayNumber) {
-        var highCarbDay =
-            "<tr><th>DAY <span class=\"week-day-number\">" + dayNumber + "</span></th></tr>" +
-            "<tr><td>Bod•ē Shake <img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'><span class='shake-img'></span></td></tr>" +
-            "<tr><td>Grilled Steak (HC) <img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'></td></tr>" +
-            "<tr><td>Bod•ē Shake <img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'><span class='shake-img'></span></td></tr>" +
-            "<tr><td>Bod•ē Burn and Whole-Wheat English Muffin with Butter Spray <img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'></td></tr>" +
-            "<tr><td>Garlic Lime Chicken (HC) <img class='was-eaten hidden' src='../images/mealEaten.png' alt='tick'></td></tr>" +
-            "<tr><td>HIGH-CARB</td></tr>" +
-            "<tr><td><span class='workout-done'></span></td></tr>";
-        return highCarbDay;
+
+    var getWorkoutData = function () {
+        var workout = "<tr>";
+        workout += "<td><span>Workout</span><img src='./images/workoutArrow.png' alt='arrow'></td>";
+        for (var i = 0; i < mealPlan.length; i++) {
+            var dailyWorkout = mealPlan[i];
+            if(dailyWorkout.dietType !== guiltFree){
+                workout += "<td>" + dailyWorkout.workoutToDo + "</td>";
+            };
+        }
+        workout += "</tr>";
+        return workout;
     };
-    var getGuiltFreeDayTemplateHtml = function(dayNumber) {
+    
+    var getGuiltFreeDayTemplateHtml = function () {
         var guiltFreeDay =
-            "<tr><th>DAY <span class=\"week-day-number\">" + dayNumber + "</span></th></tr>" +
-            "<td class=\"guiltFreeDay\" rowspan=\"5\"><img src=\"../images/guiltfreeday.png\" alt=\"guilt_free_day\">" +
-            "<img src=\"../images/smile.png\" alt=\"smile\"></td>";
+            "<td class=\"guiltFreeDay\" rowspan=\"5\"><img src=\"./images/guiltfreeday.png\" alt=\"guilt_free_day\">" +
+            "<img src=\"./images/smile.png\" alt=\"smile\"></td>";
+        console.log("in get guilt free");
         return guiltFreeDay;
     };
 
@@ -119,52 +159,95 @@ var mealPlan = [
         "day": "1",
         "dietType": "LOW CARB",
         "workoutToDo": "true",
-        "workoutDone": "true"
+        "workoutDone": "true",
+        "meals": [
+            { "mealNumber": 0, "mealId": 1, "meal": "Bod•ē Shake", "eaten": "true" },
+            { "mealNumber": 1, "mealId": 2, "meal": "Ham and Swiss Roll Ups ", "eaten": "true" },
+            { "mealNumber": 2, "mealId": 3, "meal": "Turkey Melt", "eaten": "true" },
+            { "mealNumber": 3, "mealId": 4, "meal": "Bod•ē Burn with Mozzarella and Tomato Slices ", "eaten": "true" },
+            { "mealNumber": 4, "mealId": 5, "meal": "Turkey Melt", "eaten": "false" }
+        ]
     },
     {
         "day": "2",
         "dietType": "LOW CARB",
         "workoutToDo": "true",
-        "workoutDone": "true"
+        "workoutDone": "true",
+        "meals": [
+            { "mealNumber": 0, "mealId": 1, "meal": "Bod•ē Shake", "eaten": "false" },
+            { "mealNumber": 1, "mealId": 2, "meal": "Ham and Swiss Roll Ups ", "eaten": "false" },
+            { "mealNumber": 2, "mealId": 3, "meal": "Turkey Melt", "eaten": "false" },
+            { "mealNumber": 3, "mealId": 4, "meal": "Bod•ē Burn with Mozzarella and Tomato Slices ", "eaten": "false" },
+            { "mealNumber": 4, "mealId": 3, "meal": "Turkey Melt", "eaten": "false" }
+        ]
     },
     {
         "day": "3",
         "dietType": "HIGH CARB",
         "workoutToDo": "true",
-        "workoutDone": "false"
+        "workoutDone": "false",
+        "meals": [
+            { "mealNumber": 0, "mealId": 1, "meal": "Bod•ē Shake", "eaten": "false" },
+            { "mealNumber": 1, "mealId": 5, "meal": "Grilled Steak (HC)", "eaten": "false" },
+            { "mealNumber": 2, "mealId": 1, "meal": "Bod•ē Shake", "eaten": "false" },
+            { "mealNumber": 3, "mealId": 6, "meal": "Bod•ē Burn and Whole-Wheat English Muffin with Butter Spray ", "eaten": "false" },
+            { "mealNumber": 4, "mealId": 7, "meal": "Garlic Lime Chicken (HC)", "eaten": "false" }
+        ]
     },
     {
         "day": "4",
         "dietType": "LOW CARB",
         "workoutToDo": "true",
-        "workoutDone": "false"
+        "workoutDone": "false",
+        "meals": [
+            { "mealNumber": 0, "mealId": 1, "meal": "Bod•ē Shake", "eaten": "false" },
+            { "mealNumber": 1, "mealId": 2, "meal": "Ham and Swiss Roll Ups ", "eaten": "false" },
+            { "mealNumber": 2, "mealId": 3, "meal": "Turkey Melt", "eaten": "false" },
+            { "mealNumber": 3, "mealId": 4, "meal": "Bod•ē Burn with Mozzarella and Tomato Slices ", "eaten": "false" },
+            { "mealNumber": 4, "mealId": 5, "meal": "Turkey Melt", "eaten": "false" }
+        ]
     },
     {
         "day": "5",
         "dietType": "LOW CARB",
         "workoutToDo": "true",
-        "workoutDone": "false"
+        "workoutDone": "false",
+        "meals": [
+            { "mealNumber": 0, "mealId": 1, "meal": "Bod•ē Shake", "eaten": "false" },
+            { "mealNumber": 1, "mealId": 2, "meal": "Ham and Swiss Roll Ups ", "eaten": "false" },
+            { "mealNumber": 2, "mealId": 3, "meal": "Turkey Melt", "eaten": "false" },
+            { "mealNumber": 3, "mealId": 4, "meal": "Bod•ē Burn with Mozzarella and Tomato Slices ", "eaten": "false" },
+            { "mealNumber": 4, "mealId": 5, "meal": "Turkey Melt", "eaten": "false" }
+        ]
     },
     {
         "day": "6",
         "dietType": "HIGH CARB",
         "workoutToDo": "true",
-        "workoutDone": "false"
+        "workoutDone": "false",
+        "meals": [
+            { "mealNumber": 0, "mealId": 1, "meal": "Bod•ē Shake", "eaten": "false" },
+            { "mealNumber": 1, "mealId": 5, "meal": "Grilled Steak (HC)", "eaten": "false" },
+            { "mealNumber": 2, "mealId": 1, "meal": "Bod•ē Shake", "eaten": "false" },
+            { "mealNumber": 3, "mealId": 6, "meal": "Bod•ē Burn and Whole-Wheat English Muffin with Butter Spray ", "eaten": "false" },
+            { "mealNumber": 4, "mealId": 7, "meal": "Garlic Lime Chicken (HC)", "eaten": "false" }
+        ]
     },
     {
         "day": "7",
-        "dietType": "GUILT FREE",
         "workoutToDo": "false",
-        "workoutDone": "false"
-
+        "workoutDone": "false",
+        "dietType": "GUILT FREE",
+        "meals": []
     }
 ];
 
-var mealsToImg = [
-    {
-        "mealId": "1",
-        "imgToHtml": "<img src=\"./images/shake.png\" alt=\"shake\">"
-    }
+var mealHours = [
+    "6:00 AM",
+    "9:00 AM",
+    "12:00 PM",
+    "3:00 PM",
+    "6:00 PM"
 ];
 
 
